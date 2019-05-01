5c8f3475b50fcbd709644560d3b9eee24826f95c
Torres-ssf <sergio.uft@gmail.com>
2019-05-01 15:39:46 -0300

    Fix linter errors

--- lib/board.rb
+++ lib/board.rb
@@ -1,29 +1,28 @@
+# Board class
 class Board
-
   def initialize
-     @board_hash = initHash
+    @board_hash = initHash
   end
 
-  public
-  def showBoard
+  def show_board
     puts "#{@board_hash[1]} | #{@board_hash[2]} | #{@board_hash[3]} "
     puts "#{@board_hash[4]} | #{@board_hash[5]} | #{@board_hash[6]} "
     puts "#{@board_hash[7]} | #{@board_hash[8]} | #{@board_hash[9]} "
   end
 
-  def getCell(position)
-     @board_hash[position]
+  def get_cell(position)
+    @board_hash[position]
   end
 
-  def assigningValue(position, value)
+  def assigning_value(position, value)
     @board_hash[position] = value
   end
 
   private
-  def initHash
-    new_hash = Hash.new
+
+  def init_hash
+    new_hash = {}
     (1..9).each { |i| new_hash[i] = " " }
     new_hash
   end
-
 end
