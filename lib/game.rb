822c98fdf5a39dfb28a25ef8cda67737a72beaad
Torres-ssf <sergio.uft@gmail.com>
2019-05-02 17:53:46 -0300

    Finish game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,49 +1,37 @@
 require_relative 'board'
 # Game Class
 class Game
-  def initialize(position)
+  def initialize
     @new_board = Board.new
-    @is_x = true
-    play_game(position)
+    @current_player = ""
+    @input_count = 0
+    @game_status = -1
+    play_game
   end
 
-  def play_game(position)
-    player = current_player
-    @new_board.assigning_value(position, player)
-    display
-  end
-
-  private
-
-  def current_player
-    @current_player = @is_x ? 'X' : 'O'
-  end
-
-  def display
-    puts @new_board.show_board
-    status
-  end
-
-  def status
-    player = current_player
-    p calculate_winner
-    case calculate_winner
-    when -1
-      @is_x = !@is_x
-      puts "Player #{player} is next!"
-    when 1
-      puts "Player #{player} is the winner!"
-      puts 'GAME END'
+  def play_game
+    while(@game_status == -1)
+      display_board
+      change_current_player
+      display_status
+      input = request_player_input
+      assign_player_input(input)
+      @game_status = check_game_status
+    end
+    if(@game_status == 1)
+      display_board
+      puts "Player #{@current_player} won"
     else
-      puts 'DRAWWWWWWW'
+      display_board
+      puts "It's a draw"
     end
   end
 
-  def check(index)
-    @new_board.get_cell(index)
+  def display_board
+    @new_board.show_board
   end
 
-  def calculate_winner
+  def check_game_status
     lines = [
       [1, 2, 3],
       [4, 5, 6],
@@ -54,26 +42,54 @@ class Game
       [1, 5, 9],
       [3, 5, 7]
     ]
+
     lines.each do |i|
       if (check(i[0]) == check(i[1])) && (check(i[0]) == check(i[2]))
-        return 0 if check(i[0]) == ' '
-
-        return 1
+        return 1 if(check(i[0]) == 'X' || check(i[0]) == 'O')
       end
-      return -1
     end
+    return 0 if(@input_count == 9)
+    return -1
+  end
+
+  def check(index)
+    @new_board.get_cell(index)
+  end
+
+  def change_current_player
+    @current_player = @current_player == 'X' ? 'O' : 'X'
+  end
+
+  def display_status
+    puts "Player #{@current_player} is the next to play! Make your move."
+  end
+
+  def request_player_input
+    input = gets.chomp.to_i
+    until is_valid_input(input)
+      input = gets.chomp.to_i
+    end
+    input
+  end
+
+  def assign_player_input(position)
+    @new_board.assigning_value(position, @current_player)
+    @input_count += 1
+  end
+
+  def is_valid_input(input)
+    if((input.is_a? Integer) && (input > 0 && input < 10))
+      if(check(input) == ' ')
+        return true
+      else
+        puts "Please select an available position instead"
+      end
+    else
+      puts "Wrong type of input"
+      puts "Please select an integer between 1 and 9"
+    end
+    return false
   end
 end
 
-new_game = Game.new(1)
-new_game.play_game(2)
-new_game.play_game(3)
-new_game.play_game(5)
-new_game.play_game(4)
-new_game.play_game(7)
-new_game.play_game(8)
-new_game.play_game(9)
-new_game.play_game(6)
-
-new_game.play_game(6)
-
+game = Game.new
