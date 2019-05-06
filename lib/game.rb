95ecd7a7b2d2c23dae7892e90522e9184814cac5
Torres-ssf <sergio.uft@gmail.com>
2019-05-06 18:43:58 -0300

    Refactor all code and create player class

--- lib/game.rb
+++ lib/game.rb
@@ -1,87 +1,51 @@
 require_relative 'board'
-require_relative 'helper'
+require_relative 'player'
 # Game Class
 class Game
-  include Helper
   def initialize
-    @new_board = Board.new
-    @current_player = ''
-    @input_count = 0
-    @game_status = -1
+    @board = Board.new
+    @player_1 = Player.new("Thor", "X", @board)
+    @player_2 = Player.new("Hulk", "O", @board)
+    @current_player = @player_1
   end
 
   def play_game
-    welcome_message
-    game_start_instructions
-    while @game_status == -1
-      display_board
-      change_current_player
-      display_status
-      input = request_player_input
-      assign_player_input(input)
-      @game_status = check_game_status
+    loop do
+      @board.render
+      @current_player.get_player_input
+      break if game_over?
+      switch_player
     end
-    if @game_status == 1
-      display_board
-      player_win(@current_player)
+  end
+
+  def switch_player
+    @current_player = @current_player == @player_1 ? @player_2 : @player_1
+  end
+
+  def game_over?
+    victory? || draw?
+  end
+
+  def victory?
+    if @board.winning_cases?(@current_player.symbol)
+      @board.render
+      puts "Congratulations #{@current_player.name}, you won"
+      true
     else
-      display_board
-      player_draw
+      false
     end
-    game_end_instructions
   end
 
-  private
-
-  def display_board
-    @new_board.show_board
-  end
-
-  def check_game_status
-    LINES.each do |i|
-      if (check(i[0]) == check(i[1])) && (check(i[0]) == check(i[2]))
-        return 1 if check(i[0]) == 'X' || check(i[0]) == 'O'
-      end
-    end
-    return 0 if @input_count == 9
-
-    -1
-  end
-
-  def check(index)
-    @new_board.get_cell(index)
-  end
-
-  def change_current_player
-    @current_player = @current_player == 'X' ? 'O' : 'X'
-  end
-
-  def display_status
-    player_status(@current_player)
-  end
-
-  def request_player_input
-    input = gets.chomp.to_i
-    input = gets.chomp.to_i until is_valid_input(input)
-    input
-  end
-
-  def assign_player_input(position)
-    @new_board.assigning_value(position, @current_player)
-    @input_count += 1
-  end
-
-  def is_valid_input(input)
-    if (input.is_a? Integer) && (input > 0 && input < 10)
-      if check(input) == ' '
-        return true
-      else
-        puts 'Please select an available position instead'
-      end
+  def draw?
+    if @board.full?
+      @board.render
+      puts "Too bad. It's a draw."
+      true
     else
-      puts 'Wrong type of input'
-      puts 'Please select an integer between 1 and 9'
+      false
     end
-    false
   end
 end
+
+game = Game.new
+game.play_game
---
+++ lib/player.rb
@@ -0,0 +1,32 @@
+require_relative "board"
+
+class Player
+  attr_accessor :name, :symbol
+  def initialize(name = "Nameless Player", symbol, board)
+    @name = name
+    @symbol = symbol
+    @board = board
+  end
+
+  def get_player_input
+    loop do
+      position = get_position
+      if valid_input_position?(position)
+        break if @board.assigning_value(position, @symbol)
+      end
+    end
+  end
+
+  def get_position
+    puts "#{name}(#{@symbol}): It's your turn. Make your move!"
+    gets.chomp.to_i
+  end
+
+  def valid_input_position?(position)
+    if position.is_a? Integer
+      true
+    else
+      puts "Please insert a number from 1 to 9."
+    end
+  end
+end
