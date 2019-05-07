b1e94573c981362d464a6adf10ad42ba1d5290aa
Buari Bola Ahmed <bola_ahmed2002@yahoo.com>
2019-05-07 19:19:17 +0100

    Update class to instantiate the Board and Player class

--- bin/tictactoe.rb
+++ bin/tictactoe.rb
@@ -1,4 +1,9 @@
+load 'lib/board.rb'
+load 'lib/player.rb'
 load 'lib/game.rb'
 
-tictactoe = Game.new
+board = Board.new
+player1 = Player.new('Thor', 'X', board)
+player2 = Player.new('Hulk', 'O', board)
+tictactoe = Game.new(player1, player2, board)
 tictactoe.play_game
