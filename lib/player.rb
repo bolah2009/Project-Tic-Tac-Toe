f47eef7448127bb985c7067d648359add73ce74f
Buari Bola Ahmed <bola_ahmed2002@yahoo.com>
2019-05-07 19:04:33 +0100

    Update Player class
    Player class now gets value from the ui module

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,9 @@
-require_relative "board"
+require_relative 'UI'
 
 class Player
+  include UI
   attr_accessor :name, :symbol
-  def initialize(name = "Nameless Player", symbol, board)
+  def initialize(name = 'Nameless Player', symbol, board)
     @name = name
     @symbol = symbol
     @board = board
@@ -11,24 +12,15 @@ class Player
   def get_player_input
     loop do
       position = get_position
-      if valid_input_position?(position)
-        break if @board.assigning_value(position, @symbol)
-      end
+
+      break if @board.assigning_value(position, @symbol)
     end
   end
 
   private
 
   def get_position
-    puts "#{name}(#{@symbol}): It's your turn. Make your move!"
-    gets.chomp.to_i
-  end
-
-  def valid_input_position?(position)
-    if position.is_a? Integer
-      true
-    else
-      puts "Please insert a number from 1 to 9."
-    end
+    turns(name, @symbol)
+    get_valid_input
   end
 end
